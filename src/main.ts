f7a66d9e2a7b8be8869fca8dde093f5142b03df2
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-09-29 11:45:48 +0200

    Register component globally

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,14 @@ import { createPinia } from 'pinia'
 
 import App from '@/layouts/App.vue'
 import router from '@/router'
+import LvButton from 'lightvue/button'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+//- Global component registration
+app.component('LvButton', LvButton)
 
 app.mount('#app')
