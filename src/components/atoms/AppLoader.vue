2da6c40baec84e859ee32a1e8f2a536a94947e93
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-16 16:58:22 +0200

    Add overlay when app loading

--- src/components/atoms/AppLoader.vue
+++ src/components/atoms/AppLoader.vue
@@ -1,9 +1,10 @@
 <template>
-  <LvProgressBar 
-    mode="indeterminate" 
-    color="#bb2727" 
-    :class="{ 'lv-progressbar--visible ': isLoading }" 
-  />
+  <div class="wrapper" :class="{ 'wrapper--visible ': isLoading }">
+    <LvProgressBar 
+      mode="indeterminate" 
+      color="#bb2727" 
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -14,6 +15,21 @@ import LvProgressBar from 'lightvue/progress-bar'
 const isLoading = computed(() => useLoadingStore().isLoading)
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .wrapper {
+    background-color: rgba($dark, .1);
+    z-index: 999;
+    position: fixed;
+    top: 0;
+    right: 0;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    visibility: hidden;
 
+    &--visible {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
 </style>
