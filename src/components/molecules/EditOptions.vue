347c72b193b957e14927a1ee1d3ff1f8c3e8fdc5
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2023-10-30 20:28:07 +0100

    Implement dialog to handle row(s)

---
+++ src/components/molecules/EditOptions.vue
@@ -0,0 +1,46 @@
+<template>
+  <LvDialog 
+    header="Handle row(s)" 
+    v-model="visible"
+    :style="{ 
+      width: '95%',
+      maxWidth: '400px'
+    }"
+  >
+    <BaseButton 
+      icon="x" 
+      class="lv-button--ml-10"
+      @click.stop
+      danger
+      :style="{ marginTop: '1rem !important', marginLeft: 'auto !important' }"
+    >
+      Delete
+    </BaseButton>
+  </LvDialog>
+</template>
+
+<script setup lang="ts">
+import LvDialog from 'lightvue/dialog'
+import BaseButton from '@/components/atoms/BaseButton.vue'
+import { computed } from 'vue'
+
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const emit = defineEmits<{
+  (event: 'close', value: boolean): void
+}>()
+
+const visible = computed({
+  get: () => props.isVisible,
+  set: value => emit('close', value)
+})
+</script>
+
+<style scoped>
+
+</style>
