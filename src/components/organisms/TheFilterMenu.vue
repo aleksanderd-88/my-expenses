5d879ba4aee82ff0f4c9e4b9a2fce222274d75e1
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-01-04 00:00:29 +0100

    Modify filter menu

--- src/components/organisms/TheFilterMenu.vue
+++ src/components/organisms/TheFilterMenu.vue
@@ -8,19 +8,28 @@
         @click.stop="closeFilterMenu()"
       />
           
-      <h1 class="filter-menu__headline">Filter expenses</h1>
+      <h1 class="filter-menu__headline">Expense presets</h1>
   
       <ul class="filter-menu__list">
-        <li class="filter-menu__item" v-for="item in items" :key="item.label">
+        <li 
+          class="filter-menu__item mt-2"
+          v-for="item in filteredItems" 
+          :key="item.title"
+        >
+          <h2 class="filter-menu__preset-title">
+            {{ item.title }}
+          </h2>
+
           <BaseButton
-            :icon="item.icon"
+            v-for="button in item.buttons" :key="button.label"
+            :icon="button.icon"
             icon-left
             transparent
             class="filter-menu__filter-btn"
-            :class="{ 'filter-menu__filter-btn--active': active(item.mode) }"
-            @click.stop="useTableStore().setLayoutMode(item.mode)"
+            :class="{ 'filter-menu__filter-btn--active': active(button.mode) }"
+            @click.stop="useTableStore().setLayoutMode(button.mode)"
           >
-            {{ item.label }}
+            {{ button.label }}
           </BaseButton>
         </li>
       </ul>
@@ -42,6 +51,12 @@ type ItemPropType = {
   mode: ModeTypes
 }
 
+type PresetTitlePropType = {
+  title: string
+  modes: string[],
+  buttons?: ItemPropType[]
+}
+
 const menu = ref()
 
 //- Close filter menu everytime mode changes
@@ -51,12 +66,23 @@ watch(() => useTableStore().mode, (val) => {
 })
 
 const items: ItemPropType[] = reactive([
-  { label: 'All expenses', icon: 'list', mode: 'list' },
-  { label: 'Category view', icon: 'layout-list', mode: 'category' },
-  { label: 'Paid expenses', icon: 'list-check', mode: 'paid' },
-  { label: 'Unpaid expenses', icon: 'clipboard-x', mode: 'unpaid' }
+  { label: 'Expenses', icon: 'list', mode: 'list' },
+  { label: 'Categories', icon: 'layout-list', mode: 'category' },
+  { label: 'Paid', icon: 'list-check', mode: 'paid' },
+  { label: 'Unpaid', icon: 'clipboard-x', mode: 'unpaid' }
 ])
 
+const presetTitles: PresetTitlePropType[] = reactive([
+  { title: 'Views', modes: ['list', 'category'] },
+  { title: 'Status', modes: ['paid', 'unpaid'] },
+])
+
+const filteredItems = computed(() => presetTitles.map((p: PresetTitlePropType) => {
+  return {
+    ...p,
+    buttons: items.filter(i => p.modes.includes(i.mode))
+  }
+}))
 const modifiedClass = computed(() => useAppMenu().filterMenuIsVisible && 'filter-menu--visible')
 const active = (mode: string) => useTableStore().mode === mode
 
@@ -93,14 +119,21 @@ const closeFilterMenu = () => useAppMenu().setFilterMenuVisibility(false)
     }
 
     &__list {
-      margin-top: 2rem;
+      margin-top: 4rem;
     }
 
     &__item {
       margin-bottom: 1rem;
     }
 
+    &__preset-title {
+      font-size: 1.1rem;
+      padding-bottom: .5rem;
+      border-bottom: 1px solid rgba(#000, .1);
+    }
+
     &__filter-btn {
+      margin: .5rem 0;
       width: 100% !important;
 
       &--active {
