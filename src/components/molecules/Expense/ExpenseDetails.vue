4984b95d80bfbd0289cf593d47c758a4d06aa4b9
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2024-04-06 21:01:34 +0200

    Add expense details shimmer

--- src/components/molecules/Expense/ExpenseDetails.vue
+++ src/components/molecules/Expense/ExpenseDetails.vue
@@ -1,29 +1,33 @@
 <template>
   <div class="vtl__bottom">
     <main class="vtl__bottom-content col col--flex mt-0">
-      <h2 class="sub-headline sub-headline--flex-start" v-if="paidExpensesLength">
-        Remaining expenses: {{ formatCurrency(Number(calculatedRemaingExpenses)) }}
-        <p> 
-          <span>{{ paidExpensesDetails }}</span> <b>({{ `${ formatCurrency(calculatedPaidExpenses) } of ${ formatCurrency(calculatedTotalExpense) }` }})</b>
-        </p>
-      </h2>
+      <ShimmerExpenseDetails v-if="isLoading" />
 
-      <h2 class="sub-headline sub-headline--flex-end">
-        Total expenses: {{ formatCurrency(Number(calculatedTotalExpense)) }}
-        <p v-if="addedIncome"> 
-          Remaining income: 
-          <span :style="{ color: `${ Number(addedIncome) < Number(calculatedTotalExpense) ? 
-            '#ef5350' : '#495057'}` }">
-            <b>
-              {{ formatCurrency(Number(addedIncome) - Number(calculatedTotalExpense)) }}
-            </b>
-          </span>
+      <template v-else>
+        <h2 class="sub-headline sub-headline--flex-start" v-if="paidExpensesLength">
+          Remaining expenses: {{ formatCurrency(Number(calculatedRemaingExpenses)) }}
+          <p> 
+            <span>{{ paidExpensesDetails }}</span> <b>({{ `${ formatCurrency(calculatedPaidExpenses) } of ${ formatCurrency(calculatedTotalExpense) }` }})</b>
+          </p>
+        </h2>
 
-          <span>
-            (Income: <b>{{ `${ formatCurrency(Number(addedIncome)) }` }}</b>)
-          </span>
-        </p>
-      </h2>
+        <h2 class="sub-headline sub-headline--flex-end">
+          Total expenses: {{ formatCurrency(Number(calculatedTotalExpense)) }}
+          <p v-if="addedIncome"> 
+            Remaining income: 
+            <span :style="{ color: `${ Number(addedIncome) < Number(calculatedTotalExpense) ? 
+              '#ef5350' : '#495057'}` }">
+              <b>
+                {{ formatCurrency(Number(addedIncome) - Number(calculatedTotalExpense)) }}
+              </b>
+            </span>
+
+            <span>
+              (Income: <b>{{ `${ formatCurrency(Number(addedIncome)) }` }}</b>)
+            </span>
+          </p>
+        </h2>
+      </template>
     </main>
   </div>
 </template>
@@ -33,8 +37,13 @@ import { formatCurrency } from '@/utils/formatter'
 import { computed } from 'vue'
 import { useExpenseStore } from '@/stores/expense';
 import { useIncomeStore } from '@/stores/income';
+import ShimmerExpenseDetails from '../Shimmer/ShimmerExpenseDetails.vue';
+import { useLoadingStore } from '@/stores/loader';
 
 const table = useExpenseStore().table
+const loaderStore = useLoadingStore()
+
+const isLoading = computed(() => loaderStore.isLoading)
 
 const calculatedTotalExpense = computed(() => {
   return table.rows.reduce((sum, item) => sum += item.cost, 0)
---
+++ src/components/molecules/Shimmer/ShimmerExpenseDetails.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="shimmer-expense-details">
+    <section class="shimmer-expense-details__content shimmer-expense-details__content--align-left">
+      <LvSkeleton :width="322" :height="24" />
+      <LvSkeleton :width="410" :height="18" />
+    </section>
+
+    <section class="shimmer-expense-details__content">
+      <LvSkeleton :width="316" :height="24" />
+      <LvSkeleton :width="437" :height="18" />
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .shimmer-expense-details {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+
+    &__content {
+      display: flex;
+      gap: .5rem;
+      justify-content: center;
+      flex-direction: column;
+
+      &--align-left {
+        text-align: left;
+      }
+    }
+  }
+</style>
